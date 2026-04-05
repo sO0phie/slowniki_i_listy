05505da928b2deb8e3156d7467949038a6ff48e0
sO0phie <147306231+sO0phie@users.noreply.github.com>
2026-04-05 11:46:51 +0200

    Update main.py

--- dictionery and list/main.py
+++ dictionery and list/main.py
@@ -11,6 +11,13 @@ def program_menu()->None:
     e - exsit
     w - add user
     r - data inf
+    t - add/remove user's grades
+    y - find user by ID
+    u - find user by name
+    i - delete user by ID
+    u - update user's name
+    k - update user's surname
+    l - update user's birth date
     \n
     ================================================
     """)
@@ -32,14 +39,29 @@ def main():
         elif inp == "r":
             data.print_all_data(loaded_data)
         elif inp == "t":
-           users.add_or_remove_grades(loaded_data)
+            users.add_or_remove_grades(loaded_data)
         elif inp == "y":
-            pass
+            user_id = int(input("Wpisz ID użytkownika "))
+            users.find_user_by_id(loaded_data, user_id)
         elif inp == "u":
-            pass
+            name = str(input("Wpisz imię użytkownika: "))
+            users.find_users_by_name(loaded_data, name)
         elif inp == "i":
-            pass
+            user_id = int(input("Wpisz ID użytkownika "))
+            users.delete_user_by_id(loaded_data, user_id)
         elif inp == "o":
+            user_id = int(input("Wpisz ID użytkownika "))
+            new_name = str(input("Wpisz nowe imię użytkownika "))
+            users.update_user_name(loaded_data, user_id, new_name)
+        elif inp == "k":
+            user_id = int(input("Wpisz ID użytkownika "))
+            new_surname = str(input("Wpisz nowe nazwisko użytkownika "))
+            users.update_user_surname(loaded_data, user_id, new_surname)
+        elif inp == "l":
+            user_id = int(input("Wpisz ID użytkownika "))
+            new_birth_date = str(input("Wpisz nową datę urodzenia użytkownika "))
+            users.update_user_birth_date(loaded_data, user_id, new_birth_date)
+        elif inp == "m":
             pass
         else:
             os.system("cls" if os.name == "nt" else "clear")
